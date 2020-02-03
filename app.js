b9e672b4f151dc1c97be8ac4a51cb319c6b9a15d
Jorge Fabregas <jorge.fabregas22@gmail.com>
2020-02-03 00:56:51 -0500

    register page

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+
+const users = [];
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: 'Jorge'});
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+})
+
+app.post('/login', (req, res) => {
+    
+})
+
+app.get('/register', async (req, res) => {
+    res.render('register.ejs');
+})
+
+app.post('/register', (req, res) => {
+    try {
+        const harshedPassword = bcrypt.hash(req.body.password, 5);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: harshedPassword  
+    })
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register');
+    }
+})
+
+app.listen(3000);
